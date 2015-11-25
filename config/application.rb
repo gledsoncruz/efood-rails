59ddda8783851d74fa63d4af687c6f90c7dd02bb
Gledson Cruz <gledson.cruz@gmail.com>
2015-11-25 10:46:40 -0200

    Config bower

--- config/application.rb
+++ config/application.rb
@@ -20,11 +20,6 @@ module EfoodRails
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'pt-BR'
 
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
